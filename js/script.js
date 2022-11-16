99cff80546c2df85344d65041e49d46cce4e7310
lianavez <111656889+lianavez@users.noreply.github.com>
2022-11-16 22:45:47 +0200

    dropdown change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+let nav = document.querySelector('nav');
+let dropdown = nav.querySelector('.dropdown');
+let dropdownToggle = nav.querySelector("[data-action='dropdown-toggle']");
+let navToggle = nav.querySelector("[data-action='nav-toggle']");
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+dropdownToggle.addEventListener('click', () => {
+	if (dropdown.classList.contains('show')) {
+		dropdown.classList.remove('show');
+	} else {
+		dropdown.classList.add('show');
+	}
 })
 
+navToggle.addEventListener('click', () => {
+	if (nav.classList.contains('opened')) {
+		nav.classList.remove('opened');
+	} else {
+		nav.classList.add('opened');
+	}
+})
