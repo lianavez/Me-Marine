4d5843eacf51f15d0a89d58132bc349d4c3cc4b2
lianavez <111656889+lianavez@users.noreply.github.com>
2022-12-04 15:46:25 +0200

    dropdown fix

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 let nav = document.querySelector('nav');
-let dropdown = nav.querySelector('.dropdown');
-let dropdownToggle = nav.querySelector("[data-action='dropdown-toggle']");
+let dropdowns = nav.querySelectorAll('.dropdown');
+let dropdownToggles = nav.querySelectorAll("[data-action='dropdown-toggle']");
 let navToggle = nav.querySelector("[data-action='nav-toggle']");
 
-dropdownToggle.addEventListener('click', () => {
-	if (dropdown.classList.contains('show')) {
-		dropdown.classList.remove('show');
-	} else {
-		dropdown.classList.add('show');
-	}
-})
+dropdownToggles.forEach(function(toggle, index) {
+  toggle.addEventListener('click', function() {
+    let dropdown = dropdowns[index];
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+    } else {
+      dropdown.classList.add('show');
+    }
+  });
+});
 
 navToggle.addEventListener('click', () => {
-	if (nav.classList.contains('opened')) {
-		nav.classList.remove('opened');
-	} else {
-		nav.classList.add('opened');
-	}
-})
+  if (nav.classList.contains('opened')) {
+    nav.classList.remove('opened');
+  } else {
+    nav.classList.add('opened');
+  }
+});
